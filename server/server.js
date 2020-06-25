e3bbffd4b3a4173e7cf4d563fba4463299acada8
jkrsn98 <justinkrasner98@gmail.com>
2020-06-24 21:25:25 -0400

    Update server.js
    fixed outdated comment

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
-//everytime someone connects to the server, we send a message to the client saying Hello World
+//everytime someone connects to the server
 io.on('connection', socket => {
     socket.on('message', ({name, message}) =>{
         io.emit('message', {name, message})
